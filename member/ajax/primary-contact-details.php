63c782f09f0ae169e198be4edbc1fcd170e838ab
KNisansala <kavini@sublime.lk>
2020-09-15 14:05:32 +0530

    #178 Corrections

--- member/ajax/primary-contact-details.php
+++ member/ajax/primary-contact-details.php
@@ -5,24 +5,20 @@ include_once(dirname(__FILE__) . '/../auth.php');
 
 if ($_POST['action'] == 'send_email_verification') {
 
+    require_once "Mail.php";
     $MEMBER = new Member($_POST['member_id']);
     $MEMBER->e_verification_code = rand(10000, 99999);
     $result = $MEMBER->update();
 
-    $to = '<' . $MEMBER->email . '>';
-    $subject = 'Email Verification Code - www.srilankaproperties.lk';
-    $from = 'SRI LANKA PROPERTY NOREPLY <noreply@srilankaproperties.lk>';
+    //---------------------- SERVER WEBMAIL LOGIN ------------------------
+    $host = "sg1-ls7.a2hosting.com";
+    $username = "noreply@srilankaproperties.lk";
+    $password = "u{Sb,7;wP4FK";
+    //------------------------ MAIL ESSENTIALS --------------------------------
+    $webmail = "noreply@srilankaproperties.lk";
+    $visitorSubject = "Email Verification Code - www.srilankaproperties.lk";
 
-// To send HTML mail, the Content-type header must be set
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-// Create email headers
-    $headers .= 'From: ' . $from . "\r\n" .
-            'Reply-To: ' . $from . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
-
-// Compose a simple HTML email message
+    // Compose a simple HTML email message
     $message = '<html>';
     $message .= '<body>';
     $message .= '<div  style="padding: 10px; max-width: 650px; background-color: #f2f1ff; border: 1px solid #d4d4d4;">';
@@ -35,24 +31,36 @@ if ($_POST['action'] == 'send_email_verification') {
     $message .= '<p>Thanks & Best Regards!.. <br/> www.srilankaproperties.lk<p/>';
     $message .= '<small>*Please do not reply to this email. This is an automated email & you will not receive a response.</small><br/>';
     $message .= '<span>Hotline: +94 77 455 4141 </span><br/>';
-    $message .= '<span>Email: help@srilankaproperties.lk</span>';
+    $message .= '<span>Email: noreply@srilankaproperties.lk</span>';
     $message .= '</div>';
     $message .= '</body>';
     $message .= '</html>';
 
-// Sending email
-    if (mail($to, $subject, $message, $headers)) {
 
+    $visitorHeaders = array(
+        'MIME-Version' => '1.0', 'Content-Type' => "text/html; charset=ISO-8859-1", 'From' => $webmail,
+        'To' => $MEMBER->email,
+        'Reply-To' => $webmail,
+        'Subject' => $visitorSubject
+    );
+    $smtp = Mail::factory('smtp', array(
+        'host' => $host,
+        'auth' => true,
+        'username' => $username,
+        'password' => $password
+    ));
+    $visitorMail = $smtp->send($MEMBER->email, $visitorHeaders, $message);
+    if (PEAR::isError($visitorMail)) {
+        header('Content-Type: application/json; charset=UTF8');
+        $response['status'] = 'error';
+        echo json_encode($response);
+    } else {
         header('Content-Type: application/json; charset=UTF8');
         $response['status'] = 'success';
         $response['code'] = $MEMBER->e_verification_code;
         $response['email'] = $MEMBER->email;
         echo json_encode($response);
         exit();
-    } else {
-        header('Content-Type: application/json; charset=UTF8');
-        $response['status'] = 'error';
-        echo json_encode($response);
     }
 }
 
@@ -92,16 +100,16 @@ if ($_POST['action'] == 'change_email') {
     $subject = 'Email Verification Code - www.srilankaproperties.lk';
     $from = 'SRI LANKA PROPERTY NOREPLY <noreply@srilankaproperties.lk>';
 
-// To send HTML mail, the Content-type header must be set
+    // To send HTML mail, the Content-type header must be set
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-// Create email headers
+    // Create email headers
     $headers .= 'From: ' . $from . "\r\n" .
-            'Reply-To: ' . $from . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
+        'Reply-To: ' . $from . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
 
-// Compose a simple HTML email message
+    // Compose a simple HTML email message
     $message = '<html>';
     $message .= '<body>';
     $message .= '<div  style="padding: 10px; max-width: 650px; background-color: #f2f1ff; border: 1px solid #d4d4d4;">';
@@ -120,7 +128,7 @@ if ($_POST['action'] == 'change_email') {
     $message .= '</html>';
 
 
-// Sending email
+    // Sending email
     if (mail($to, $subject, $message, $headers)) {
 
         header('Content-Type: application/json; charset=UTF8');
